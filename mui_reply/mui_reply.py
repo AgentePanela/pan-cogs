aa88897d3c1e15c8cc58a74df28630193d90ec7c
AgentePanela <agentepanela@gmail.com>
2024-12-17 17:57:36 -0300

    change user id

--- mui_reply/mui_reply.py
+++ mui_reply/mui_reply.py
@@ -14,22 +14,29 @@ class mui_reply(commands.Cog):
         # if is not a bot
         if message.author.id == self.target_user_id and not message.author.bot:
             try:
-                await message.channel.send(self.image_url)
+                await message.channel.reply(self.image_url)
             except discord.HTTPException as e:
                 print(f"Error sending message: {e}")
 
     @commands.command()
     @commands.is_owner()
-    async def set_image(self, ctx, url: str):
+    async def mset_image(self, ctx, url: str):
         """Change image reply."""
         self.image_url = url
         await ctx.send(f"Image reply changed: {url}")
 
     @commands.command()
     @commands.is_owner()
-    async def get_image(self, ctx):
+    async def mget_image(self, ctx):
         """See actual image reply."""
-        await ctx.send(f"Acuta image set: {self.image_url}")
+        await ctx.send(f"Actuta image is (image)[{self.image_url}]")
+
+    @commands.command()
+    @commands.is_owner()
+    async def mset_user(self, ctx, userid: str):
+        """Change user reply (use id)."""
+        self.target_user_id = userid
+        await ctx.send(f"User changed to <@{self.target_user_id}>")
 
 async def setup(bot):
     await bot.add_cog(mui_reply(bot))
